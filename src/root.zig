d3d3de293f6de72e9ed0b3c038f2dcc99d80f834
Loris Cro <kappaloris@gmail.com>
2024-02-23 12:48:20 +0100

    polishing file structure

--- src/root.zig
+++ src/root.zig
@@ -1,9 +1,28 @@
-const ziggy = @import("ziggy.zig");
-
-pub const schema = @import("schema.zig");
-pub usingnamespace ziggy;
+pub const Tokenizer = @import("ziggy/Tokenizer.zig");
+pub const Parser = @import("ziggy/Parser.zig");
+pub const Value = @import("ziggy/Value.zig");
+pub const Ast = @import("ziggy/Ast.zig");
+pub const Diagnostic = @import("ziggy/Diagnostic.zig");
+pub const parseLeaky = Parser.parseLeaky;
 
 test {
-    _ = schema;
-    _ = ziggy;
+    _ = Tokenizer;
+    _ = Parser;
+    _ = Diagnostic;
+    _ = Ast;
+    _ = Value;
+}
+
+pub const schema = struct {
+    pub const Diagnostic = @import("schema/Diagnostic.zig");
+    pub const Tokenizer = @import("schema/Tokenizer.zig");
+    pub const Schema = @import("schema/Schema.zig");
+    pub const Ast = @import("schema/Ast.zig");
+};
+
+test {
+    _ = schema.Diagnostic;
+    _ = schema.Tokenizer;
+    _ = schema.Schema;
+    _ = schema.Ast;
 }
