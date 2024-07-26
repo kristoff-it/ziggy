9f26cc6bc80c44d91d1ebc2f6aef4c3064170355
Ethan Holz <ethan.holz@thoriumworks.com>
2024-07-26 16:38:21 -0500

    feat: add devShell to Nix flake

--- flake.nix
+++ flake.nix
@@ -11,13 +11,16 @@
   outputs = inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }:
-      {
+      perSystem = { pkgs, ... }:
+      rec {
         packages.default = pkgs.stdenv.mkDerivation {
             name = "ziggy";
             version = "0.0.0";
             src = ./.;
             nativeBuildInputs = [ pkgs.zig.hook ];
+        };
+        devShells.default = pkgs.mkShell {
+            buildInputs = [ packages.default.nativeBuildInputs ];
         };
       };
     };
