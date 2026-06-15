ea94ed1353d6ae4f23b361a58f6e88ce93615579
Loris Cro <kappaloris@gmail.com>
2026-06-16 01:57:54 +0200

    type checker: add enum failing test case

--- tests/schema/type-checker/loops/root.zig
+++ tests/schema/type-checker/loops/root.zig
@@ -2,29 +2,38 @@ const ziggy = @import("ziggy");
 
 pub const RootOk = struct {
     a: MyStruct,
-};
 
-const MyStruct = struct {
-    b: MyUnion,
-};
+    const MyStruct = struct {
+        b: MyUnion,
 
-const MyUnion = union(enum) {
-    x: bool,
-    y: []RootOk,
+        const MyUnion = union(enum) {
+            x: bool,
+            y: []RootOk,
+        };
+    };
 };
-
-// ------
 
 pub const RootErr = struct {
     a: MyStructErr,
+
+    const MyStructErr = struct {
+        b: MyUnionErr,
+
+        // wrongly untagged
+        const MyUnionErr = union {
+            x: bool,
+            y: []RootOk,
+        };
+    };
 };
 
-const MyStructErr = struct {
-    b: MyUnionErr,
-};
+pub const RootErr1 = struct {
+    a: MyStructErr1,
 
-// wrongly untagged
-const MyUnionErr = union {
-    x: bool,
-    y: []RootOk,
+    const MyStructErr1 = struct {
+        b: MyUnionErr1,
+
+        // wrongly an enum
+        const MyUnionErr1 = enum { x, y };
+    };
 };
